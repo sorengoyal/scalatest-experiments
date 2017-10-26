1cbd716c766ad5b75ba2f4ef5f1ab3eef6fe7b04
Soren Goyal <sogoyal@expedia.com>
2017-10-26 15:26:08 -0500

    madechanges to simplify sharing of files

--- build.sbt
+++ build.sbt
@@ -7,5 +7,4 @@ scalaVersion := "2.11.8"
 libraryDependencies += "org.scalactic" %% "scalactic" % "3.0.4"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.4" % "test"
 libraryDependencies += "org.seleniumhq.selenium" % "selenium-java" % "2.35.0"
-parallelExecution in Test := false // the default in sbt
         
--- src/test/scala/tests/BaseSpec.scala
+++ src/test/scala/tests/BaseSpec.scala
@@ -1,25 +1,14 @@
 package tests
 
-import java.net.URL
-
-import org.openqa.selenium.remote.{DesiredCapabilities, RemoteWebDriver}
 import org.scalatest._
-import org.scalatest.selenium.{Chrome, WebBrowser}
+import org.scalatest.selenium.Chrome
 /**
   * Created by sogoyal on 10/25/17.
   */
 class BaseSpec
   extends AsyncFlatSpec
-  with WebBrowser
-  with GivenWhenThen
-  with BeforeAndAfterEach
+  with Chrome
   with Matchers
   with ParallelTestExecution {
 
-  val capability = DesiredCapabilities.chrome()
-  implicit val webDriver = new RemoteWebDriver(new URL("http://172.17.137.155:4444/wd/hub"), capability)
-  val home = "http://expedia.com/"
-  override def afterEach() {
-    quit()
-  }
 }
